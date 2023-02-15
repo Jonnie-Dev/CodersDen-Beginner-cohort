d4e749ffa8d0ce57d33e16555c7c2b3b846c45f2
Jonnie-Dev <jtech2096@gmail.com>
2023-02-15 01:02:31 +0100

    Day 2 of 30daysofjavascript

---
+++ Day 2/index.js
@@ -0,0 +1,18 @@
+// Exercise 1
+
+let challenge = "30 Days Of Javascript"
+console.log(challenge);
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(0,2))
+console.log(challenge.slice(0, 21));
+console.log(challenge.includes("script"))
+console.log(challenge.split(" "));
+let task11 = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(",")
+let task12 = challenge.replace("Javascript", "Python")
+
+console.log(challenge.lastIndexOf("a"))
+
+
+// console.log(task12);
