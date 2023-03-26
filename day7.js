60afaa8b74a80cf8d51d7bb4b7c15ffb1369f2fe
Jonnie-Dev <jtech2096@gmail.com>
2023-03-27 00:57:14 +0100

    day 7

---
+++ day7.js
@@ -0,0 +1,32 @@
+let numChar = 5
+let numIDs = 6
+
+
+const randomMac = "XX:XX:XX:XX:XX:XX".replace(/X/g, function() {
+    return "0123456789ABCDEF".charAt(Math.floor(Math.random() * 16))
+  });
+
+  console.log(randomMac)
+
+
+  var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+  console.log(ip)
+
+  const rgbColorGenerator =() => {
+    const randomColor = () => Math.floor(Math.random() * 255) +1
+    return `rgb(${randomColor()} ${randomColor()} ${randomColor()})`
+  }
+
+  console.log(rgbColorGenerator())
+  
+function isPrime(num){
+  if((num <= 1)) return false
+  if (num == 2 || num == 3) return true
+  for (let i = 2; i < num; i++) {
+    console.log(num,i)
+    if (num % i === 0) return false
+  }
+  return true
+}
+
+console.log(isPrime(2))
