fd65ec5f4c6252972cdbbe234908cfb2dffbb7b3
Jonnie-Dev <jtech2096@gmail.com>
2023-03-27 02:13:35 +0100

    day 8 + dir org.

---
+++ day8.js
@@ -0,0 +1,62 @@
+const users = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+    ];
+
+function signUp(username, email, password) {
+    for (let user of users) {
+        if (user.username == username) {
+            return `${username} already exists`
+        }
+    }
+
+    users.push({
+        _id: 'random',
+        username: username,
+        email: email,
+        password: password,
+        createdAt: Date(),
+        isLoggedIn: false
+    })
+}
+
+console.log(signUp("Brook","james@brook.com", '12121'))
+console.log(users);
